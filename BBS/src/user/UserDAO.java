cd1776b2ec138f0845b9b4769b6744a593754e9e
nidoos <hsoobin21@gmail.com>
2021-07-15 00:34:37 +0900

    jsp 복습(db연동, jdbc, 페이지)

---
+++ BBS/src/user/UserDAO.java
@@ -0,0 +1,45 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement psmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPW = "root";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "SELECT userPassword FROM USER WHERE userID=?";
+		try {
+			psmt = conn.prepareStatement(SQL);
+			psmt.setString(1, userID);
+			rs=psmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; //로그인성공
+				}else
+					return 0; //비밀번호 불일치
+			}
+			return -1; //id없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+}
